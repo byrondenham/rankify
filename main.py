c7bccc891e7e89e89216fac77e5c5fe27c22390d
Byron Denham <me@byrondenham.xyz>
2023-12-11 00:20:21 +0000

    pairs now shown randomly

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 import itertools
 import random
+import json
 
 dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path)
@@ -31,22 +32,46 @@ def get_user_preference(pair):
     choice = input().strip().lower()
     return pair[0] if choice == pair[0].lower() else pair[1]
 
-def pairwise_comparison(playlist_id):
+def load_progress(file_path):
+    try:
+        with open(file_path, 'r') as file:
+            return json.load(file)
+    except (FileNotFoundError, json.JSONDecodeError):
+        return {}
+
+def save_progress(file_path, progress):
+    with open(file_path, 'w') as file:
+        json.dump(progress, file)
+
+def pairwise_comparison(playlist_id, progress_file):
     # Get playlist tracks
     playlist_tracks = get_playlist_tracks(playlist_id)
 
-    # Generate all possible pairs of songs
-    song_pairs = list(itertools.combinations(playlist_tracks, 2))
+    # Shuffle the list of songs
+    random.shuffle(playlist_tracks)
+
+    # Load progress from the file
+    progress = load_progress(progress_file)
 
     # Initialise rankings
-    rankings = {song: 0 for song in playlist_tracks}
+    rankings = progress.get('rankings', {song: 0 for song in playlist_tracks})
 
-    # Collect user preferences for each pair
-    for pair in song_pairs:
-        preference = get_user_preference(pair)
+    # Get all possible pairs
+    all_pairs = list(itertools.combinations(playlist_tracks, 2))
 
-        # Update rankings based on user preference
-        rankings[preference] += 1
+    # Continue presenting random pairs until all pairs are ranked
+    while all_pairs:
+        pair = random.choice(all_pairs)
+        all_pairs.remove(pair)
+
+        if pair not in progress.get('pairs_seen', []):
+            preference = get_user_preference(pair)
+            rankings[preference] += 1
+            
+            # Update the progress
+            progress['pairs_seen'] = progress.get('pairs_seen', []) + [pair]
+            progress['rankings'] = rankings
+            save_progress(progress_file, progress)
 
     # Sort songs based on rankings
     sorted_rankings = sorted(rankings.items(), key=lambda x: x[1], reverse=True)
@@ -54,7 +79,8 @@ def pairwise_comparison(playlist_id):
 
 # Example usage
 playlist_id = get_user_playlist_id()
-final_ranking = pairwise_comparison(playlist_id)
+progress_file = 'progress.json' # Specify the file path where progress will be saved
+final_ranking = pairwise_comparison(playlist_id, progress_file)
 
 print("Final Ranking:")
 for song, score in final_ranking:
